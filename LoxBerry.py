c391f9383abbba18fb63d3b3671676f9ad179d04
alxgross <49441343+alxgross@users.noreply.github.com>
2020-10-30 16:59:01 +0100

    Adding the MQTT sending

--- LoxBerry.py
+++ LoxBerry.py
@@ -1,17 +1,60 @@
 import asyncio
+from gmqtt import Client as MQTTClient #https://github.com/wialon/gmqtt
 
 class LoxBerry:
-    """
+    """ Class representing the LoxBerry - attached via MQTT 
+
     Instance Attributes:
-        ip
-        port
+        broker_host
+        broker_port
+        mqtt_client 
     """
-    def __init__(self, ip, port):
-        self.ip, self.port = ip, port
+    #class attribute
+
+    def on_subscribe(self, client, mid, qos, properties):
+        print("Subscription setup")
+        
+        
+    def on_message(self, client, topic, payload, qos, properties):
+        print("message received")
+        
+    def on_connect(self, client, flags, rc, properties):
+        print('[CONNECTED {}]'.format(client._client_id))
     
+### Instance Methods
+    async def connect(self):
+        try:
+            await self.mqtt_client.connect(self.broker_host,self.broker_port)            #establish connection
+        except:
+            raise ConnectionError("Connection to Broker could not be setup")
+        
+        
+    # Takes anything coming on the queue passed and publishes to MQTT
     async def MQTTsender(self, queue: asyncio.Queue):
         while True:
-            msg = await queue.get()
-            print("MQTT sender: ", msg.appliance, msg.value)
+            msg = await queue.get()     #get Message to publish from Queue
+            ret = self.mqtt_client.publish('ALXTEST/A', msg.value)  
+            print("MQTT sender: ", msg.appliance, msg.value, ret)
             await asyncio.sleep(2)
+            
+    
+    
+    # Subscribes to a topic and then fills a given queue with messages
+    async def MQTTreceiver(self, queue: asyncio.Queue):
+        self.mqtt_client.subscribe('ALXTEST/A')
+        print("Subscribed")
+        
+    
+    # Constructor
+    def __init__(self, host, port, client_id):
+        self.broker_host, self.broker_port = host, port
+        self.client_id = client_id
+        self.mqtt_client = MQTTClient(self.client_id)       #create client object
+        
+        # Assign callback functions
+        self.on_subscribe = self.on_subscribe
+        self.on_message = self.on_message
+        self.on_connect = self.on_connect
+        
+        
         
--- loxb_websocket_mqtt_bridge.py
+++ loxb_websocket_mqtt_bridge.py
@@ -3,6 +3,7 @@
 # Author: Alx G
 # Licence: Apache 2.0
 import asyncio
+#import signal
 from settings import Env
 from LoxMiniserver import LoxMiniserver
 from LoxBerry import LoxBerry
@@ -10,19 +11,32 @@ from LoxBerry import LoxBerry
 env_lox = Env("LOX_")
 env_lb = Env("MQTT_")
 
+def ask_exit(*args):
+    STOP.set()
+
 if __name__ == '__main__':
     myLox = LoxMiniserver(env_lox.ip, env_lox.port, env_lox.user, env_lox.password)
-    loxBerry = LoxBerry(env_lb.ip, env_lb.port)
-    print(env_lb.ip, env_lb.port)
-    messageQueue = asyncio.Queue() # Creates a FIFO queue for messages
-    
+    loxBerry = LoxBerry(env_lb.broker_host, env_lb.broker_port, "alx_sender1")
+    loxBerry_listen = LoxBerry(env_lb.broker_host, env_lb.broker_port, "alx_listener1")
+    msgQ_lox2lb = asyncio.Queue() # Creates a FIFO queue for messages
+    msgQ_lb2lox = asyncio.Queue() # Creates a FIFO queue for messages
+
+    STOP = asyncio.Event()
+
     loop = asyncio.get_event_loop()
+    
+#     loop.add_signal_handler(signal.SIGINT, ask_exit)
+#     loop.add_signal_handler(signal.SIGTERM, ask_exit)
+    
     loop.run_until_complete(myLox.connect()) # Connect to Miniserver via Websockets
+    loop.run_until_complete(loxBerry.connect())
+    loop.run_until_complete(loxBerry_listen.connect())
 
     # Start listener and heartbeat 
     tasks = [
-        asyncio.ensure_future(myLox.receiver(messageQueue)), # Start the receiver for Loxone messages
-        asyncio.ensure_future(loxBerry.MQTTsender(messageQueue)) # Start the MQTT sender
+        asyncio.ensure_future(myLox.receiver(msgQ_lox2lb)), # Start the receiver for Loxone messages
+        asyncio.ensure_future(loxBerry.MQTTsender(msgQ_lox2lb)), # Start the MQTT sender
+        asyncio.ensure_future(loxBerry_listen.MQTTreceiver(msgQ_lb2lox))
     ]
 
     loop.run_until_complete(asyncio.wait(tasks))
