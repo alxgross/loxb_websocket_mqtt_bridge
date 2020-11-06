9f5f079bd832bf0f03cd960478987f173cd6d161
alxgross <49441343+alxgross@users.noreply.github.com>
2020-11-06 14:49:46 +0100

    List of dependencies and sample .env

---
+++ env-tmpl.py
@@ -0,0 +1,15 @@
+#Ensuring to not include async/sync issues
+PYTHONASYNCIODEBUG=1
+
+
+#Values for loxone access
+LOX_USER = "loxberry"
+LOX_PASSWORD = "abc"
+LOX_IP = "192.168.1.1"
+LOX_PORT = "80"
+
+LOX_CLIENT_UUID = "093302e1-02b4-603c-ffa4e5e000d80cfd"
+LOX_MINISERVER_NAME = "miniserver1"
+
+MQTT_BROKER_HOST = "localhost"
+MQTT_BROKER_PORT = "1883"
